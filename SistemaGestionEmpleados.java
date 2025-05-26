65e5845c9e4b91c1c2868f8b797702746245819e
AGARCIA54-prog <agarcia868@alu.fpsanantonio.com>
2025-05-26 09:50:27 +0200

    Modificacion de la visibilidad de los atributos de empleados, sus setters y getters y las modificaciones a la clase main para su funcionamiento

--- SistemaGestionEmpleados.java
+++ SistemaGestionEmpleados.java
@@ -1,5 +1,22 @@
 import java.util.Scanner;
 
+class Empleado {
+    String nombre;
+    String puesto;
+    double salario;
+
+    public Empleado(String nombre, String puesto, double salario) {
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Puesto: " + puesto + ", Salario: " + salario;
+    }
+}
+
 public class SistemaGestionEmpleados {
 
     private Empleado[] empleados;
